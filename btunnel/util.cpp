da41743df8f4e735d3526456a35f1f5e8ebf9784
Allen Porter <allen.porter@gmail.com>
2007-11-11 04:49:47 +0000

    Remove old cruft.

--- btunnel/util.cpp
+++ btunnel/util.cpp
@@ -8,38 +8,10 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <iostream>
-#include "listener.h"
 
 using namespace std;
 
 namespace btunnel {
-
-PacketDumper::PacketDumper(size_t max_length) : max_length_(max_length) {
-  callback_ = ythread::NewCallback(this, &PacketDumper::DumpPacket);
-}
-
-PacketDumper::~PacketDumper() {
-  delete callback_;
-}
-
-ReceivedCallback* PacketDumper::callback() {
-  return callback_;
-}
-
-void PacketDumper::DumpPacket(btunnel::packet_info* info) {
-  cout << "Received from "
-       << inet_ntoa(info->sender.sin_addr)
-       << ":" << info->sender.sin_port << endl;
-  int num = (info->length > max_length_) ? max_length_
-                                         : info->length;
-  for (int i = 0; i < num; ++i) {
-    printf(" %02x", info->data[i]);
-  }
-  if (info->length > max_length_) {
-    cout << " ...";
-  }
-  cout << endl;
-}
 
 bool GetHostPort(const string& host_port, string* host, uint16_t* port) {
   size_t c = host_port.find(":");
--- btunnel/util.h
+++ btunnel/util.h
@@ -5,28 +5,8 @@
 
 #include <string>
 #include <sys/types.h>
-#include "listener.h"
 
 namespace btunnel {
-
-// Writes contents of received by a Listener to stdout.
-class PacketDumper {
- public:
-  // The maximum number of bytes displayed from a packet.  Packets greater
-  // than max_length are truncated.
-  PacketDumper(size_t max_length);
-  ~PacketDumper();
-
-  // Callback that can be passed to a listener thread to dump the contents
-  // of a packet when invoked.
-  ReceivedCallback* callback();
-
- private:
-  void DumpPacket(btunnel::packet_info* info);
-
-  ReceivedCallback* callback_;
-  size_t max_length_;
-};
 
 // Parses a host:port string into individual components.
 bool GetHostPort(const std::string& host_port,
