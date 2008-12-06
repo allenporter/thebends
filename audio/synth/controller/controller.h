b2664e308fa4894b6254c0c6d43bbf846a522478
Allen Porter <allen.porter@gmail.com>
2008-12-06 21:30:45 +0000

    Header comment.

--- audio/synth/controller/controller.h
+++ audio/synth/controller/controller.h
@@ -1,5 +1,7 @@
 // controller.h
 // Author: Allen Porter <allen@thebends.org>
+//
+// The controller module returns samples and drives the oscillator and envelope.
 
 namespace oscillators { class Oscillator; }
 namespace envelope { class Envelope; }
@@ -10,9 +12,11 @@ class Controller {
  public:
   Controller(int sample_rate);
 
+  // Volume [0.0, 1.0]
   float volume();
   void set_volume(float volume);
 
+  // The envelope and oscillator must be set before generating samples.
   void set_oscillator(oscillators::Oscillator* oscillator);
   void set_volume_envelope(envelope::Envelope* envelope);
 
